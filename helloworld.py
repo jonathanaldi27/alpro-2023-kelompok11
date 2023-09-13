0aa7916bc877703084637ab18c13a29c53c5837c
ghifaryabrarrabbani <ghifary.abrar.rabbani-2021@ftmm.unair.ac.id>
2023-09-13 18:10:54 +0700

    initial commit helloworld

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,1 @@
-def CheckNumber(bilangan):
-    isNumber = bilangan.isnumeric()
-    return isNumber
-
-if __name__ == 'Main':
-    bilangan = input("Masukan bilangan:")
-    if(CheckNumber(bilangan)):
-        print(bilangan)
-    else:
-        print("Masukan angka!")
+print("HALO INI NYOBAIN")
